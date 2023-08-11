40c3a0eeeb5191fb45dc9adcae88dedcb01f10e4
Canabady Irupathimoondru <canabady2023@gmail.com>
2023-08-11 13:25:32 +0530

    Added utils-fakeUsers

---
+++ src/utils/fakeUsers.ts
@@ -0,0 +1,13 @@
+export const fakeUsers = [
+  { name: "DJDays", xp: 424, isCurrentUser: false },
+  { name: "Sengothai", xp: 378, isCurrentUser: false },
+  { name: "Poongothai", xp: 249, isCurrentUser: false },
+  { name: "Dora", xp: 216, isCurrentUser: false },
+  { name: "Azhakku", xp: 211, isCurrentUser: false },
+  { name: "Thenmozhi", xp: 177, isCurrentUser: false },
+  { name: "Kanimozhi", xp: 152, isCurrentUser: false },
+  { name: "Valarmathi", xp: 87, isCurrentUser: false },
+  { name: "Vaanmathi", xp: 82, isCurrentUser: false },
+  { name: "Diego", xp: 77, isCurrentUser: false },
+  { name: "Carl", xp: 10, isCurrentUser: false },
+] as const;
